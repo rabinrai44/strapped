0b67cbd7608beb8244f756e4d1ec5229bbe57e75
Rabin <rabin@Rabins-MBP.att.net>
2017-10-20 13:23:20 -0400

    Style footer and post navigation

--- footer.php
+++ footer.php
@@ -15,17 +15,19 @@
 	</div><!-- .container -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'strapped' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'strapped' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'strapped' ), 'strapped', '<a href="http://rabinrai.com">Rabin R</a>' );
-			?>
-		</div><!-- .site-info -->
+		<div class="container">
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'strapped' ) ); ?>"><?php
+					/* translators: %s: CMS name, i.e. WordPress. */
+					printf( esc_html__( 'Proudly powered by %s', 'strapped' ), 'WordPress' );
+				?></a>
+				<span class="sep"> | </span>
+				<?php
+					/* translators: 1: Theme name, 2: Theme author. */
+					printf( esc_html__( 'Theme: %1$s by %2$s.', 'strapped' ), 'strapped', '<a href="http://rabinrai.com">Rabin R</a>' );
+				?>
+			</div><!-- .site-info -->
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
